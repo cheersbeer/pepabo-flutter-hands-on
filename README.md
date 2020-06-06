2108f6757417f2e60e05bab3b54a8d3cb5d9bc48
lollypop3103 <lollypop3103@gmail.com>
2020-06-06 13:22:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# flutter_hands_on
+Sample Flutter Application.
+
+## Getting Started
+
+### Clone
+```
+$ git clone git@github.com:pepabo/flutter-hands-on.git
+```
+
+### Put Your Token on `.env`
+```
+TOKEN='your_access_token'
+```
+
+You can find your token at [SUZURI Developer](https://suzuri.jp/developer/apps)
+
+### Get Dependencies and Run
+```
+$ flutter pub get
+$ flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// main()はFlutterアプリケーションのエントリポイントです
+// main()の中で、runAppにルートとなるウィジェットを格納して呼ぶ必要があります
+void main() async {
+  await DotEnv().load('.env');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SUZURI"),
+      ),
+      body: Center(
+        child: Text("Hello, SUZURI!"),
+      ),
+    );
+  }
+}
